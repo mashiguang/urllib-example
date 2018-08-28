daf00e25ef71f7e6c87af04a97e8faae6f58bd89
mashiguang <mashiguang0833@sina.com>
2018-08-28 23:32:06 +0800

    beautifulsoup

--- src/beautiful_soup_test.py
+++ src/beautiful_soup_test.py
@@ -7,6 +7,9 @@ from bs4 import BeautifulSoup
 response = urllib.request.urlopen('https://www.v2ex.com/')
 html = response.read().decode('utf-8')
 
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, 'html.parser')
 
-print(soup.select('span.item_hot_topic_title'))
+# print(soup.find_all('span', 'item_hot_topic_title'))
+
+for span in soup.find_all('span', 'item_hot_topic_title'):
+    print(span.find('a').string)
