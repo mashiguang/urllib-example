99142887d589c5d821bb01853009e538d38a668c
mashiguang0833@sina.com <mashiguang0833@sina.com>
2018-08-29 11:12:49 +0800

    beautifulsoup

--- src/beautiful_soup_test.py
+++ src/beautiful_soup_test.py
@@ -11,5 +11,15 @@ soup = BeautifulSoup(html, 'html.parser')
 
 # print(soup.find_all('span', 'item_hot_topic_title'))
 
-for span in soup.find_all('span', 'item_hot_topic_title'):
+for span in soup.find_all('span', 'item_title'):
     print(span.find('a').string)
+
+for img in soup.find_all('img', 'avatar'):
+    print(img.parent['href'])
+
+for tr in soup.select('div.item table tr'):
+    member = tr.find('img', 'avatar').parent['href']
+    title = tr.select('span.item_title a')[0].string
+    print('%-32s %s' % (member, title))
+
+
